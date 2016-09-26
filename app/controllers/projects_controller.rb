7ef57a7b991e10b8202feccb9421dea196eb8c31
Jinho Hyeon <neverlish@gmail.com>
2016-09-26 15:43:20 +0900

    User registration

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
 	before_action :set_project, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 	def index
 		@projects = Project.all
 		@products = Project.where(category: "Product")
